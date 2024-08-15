3cf5b8ed14766499ba7f1990b755f870fda4dcfa
Abdelrahman Omar <abdelrahman29147@gmail.com>
2024-08-15 11:21:31 +0300

    feat/add-refund-topics

--- pos-gateway/src/main/java/com/poc/pos_gateway/common/Topics.java
+++ pos-gateway/src/main/java/com/poc/pos_gateway/common/Topics.java
@@ -3,4 +3,6 @@ package com.poc.pos_gateway.common;
 public class Topics {
     public static final String PAYMENT_ORDER_REQUEST = "payment-requests";
     public static final String PAYMENT_ORDER_RESPONSE = "payment-responses";
+    public static final String PAYMENT_REFUND_REQUEST = "payment-refund-requests";
+    public static final String PAYMENT_REFUND_RESPONSE = "payment-refund-responses";
 }
